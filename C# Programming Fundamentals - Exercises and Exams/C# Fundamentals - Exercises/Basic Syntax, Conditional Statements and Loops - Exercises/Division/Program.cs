d62ec07df82979e1ab57ca7e8baeb94717506090
Daniel Nikolov <kace123@abv.bg>
2021-09-05 14:36:42 +0300

    Exercises

---
+++ C# Programming Fundamentals - Exercises and Exams/C# Fundamentals - Exercises/Basic Syntax, Conditional Statements and Loops - Exercises/Division/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            
+            int maxDivisible = 0;
+
+            if (number % 2 == 0) maxDivisible = 2;
+            if (number % 3 == 0) maxDivisible = 3;
+            if (number % 6 == 0) maxDivisible = 6;
+            if (number % 7 == 0) maxDivisible = 7;
+            if (number % 10 == 0) maxDivisible = 10;
+
+            if (number % 2 == 0 && number % 3 == 0) maxDivisible = 6;
+            if (number % 2 == 0 && number % 10 == 0) maxDivisible = 10;
+
+            if (maxDivisible == 0)
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by {maxDivisible}");
+            }
+        } 
+    }
+}
---
+++ C# Programming Fundamentals - Exercises and Exams/C# Fundamentals - Exercises/Basic Syntax, Conditional Statements and Loops - Exercises/StrongNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StrongNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int[] numbers = new int[number.Length];
+            int sum = 0;
+            for (int i = 0; i < number.Length; i++)
+            {
+                numbers[i] = int.Parse(number[i].ToString());
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int result = numbers[i];
+                if (result == 0) result = 1;
+                for (int a = numbers[i] - 1; a >= 1; a--)
+                {
+                    result = result * a;
+                }
+                sum += result;
+            }
+
+            if (sum.ToString() == number)
+            {
+                Console.WriteLine("yes");
+            }
+            else Console.WriteLine("no");
+        }
+    }
+}
