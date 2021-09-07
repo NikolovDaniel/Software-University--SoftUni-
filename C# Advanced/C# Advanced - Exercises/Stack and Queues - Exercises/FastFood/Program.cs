5f4085cd33a856a72eb719e0116267768d81ed3a
Daniel Nikolov <kace123@abv.bg>
2021-09-07 08:13:20 +0300

    Added FastFood exercise

---
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercises/FastFood/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FastFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantityFood = int.Parse(Console.ReadLine());
+
+            int[] arrayOrders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> orders = new Queue<int>(arrayOrders);
+
+            Console.WriteLine(orders.Max());
+
+            while (orders.Count > 0)
+            {
+                if (quantityFood - orders.Peek() < 0)
+                {
+                    break;
+                }
+                else
+                {
+                    quantityFood -= orders.Dequeue();
+                }
+            }
+
+            if (orders.Count > 0)
+            {
+                Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+            }
+            else
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
