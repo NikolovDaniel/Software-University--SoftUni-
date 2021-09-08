f43a663ee038b6da345336485dc2ab5e6f905223
Daniel Nikolov <kace123@abv.bg>
2021-09-08 21:06:02 +0300

    Added exercise to Stacks and Queues - Exercises

---
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercises/BalancedParentheses/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BalancedParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> parentheses = new Stack<char>();
+
+            foreach (var symbol in input)
+            {
+
+                if (parentheses.Any())
+                {
+
+                    char checker = parentheses.Peek();
+
+                    if (checker == '(' && symbol == ')')
+                    {
+                        parentheses.Pop();
+                        continue;
+                    }
+                    else if (checker == '[' && symbol == ']')
+                    {
+                        parentheses.Pop();
+                        continue;
+                    }
+                    else if (checker == '{' && symbol == '}')
+                    {
+                        parentheses.Pop();
+                        continue;
+                    }
+                }
+
+                parentheses.Push(symbol);
+            }
+
+            if (parentheses.Count == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
