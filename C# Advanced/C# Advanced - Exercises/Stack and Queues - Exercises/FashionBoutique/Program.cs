ad441b51ae2053e9fc5918fecd23cd20c7aae563
Daniel Nikolov <kace123@abv.bg>
2021-09-08 21:00:39 +0300

    Added exercises to Stacks and Queues - Exercises

---
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercises/FashionBoutique/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> racks = new Stack<int>(clothes.Reverse());
+
+            int capacity = int.Parse(Console.ReadLine());
+            int capacityVariable = capacity;
+
+            int racksCount = 0;
+
+            while (racks.Count > 0)
+            {
+                if (capacityVariable - racks.Peek() < 0)
+                {
+                    racksCount++;
+                    capacityVariable = capacity;
+                }
+                else if (capacityVariable - racks.Peek() == 0)
+                {
+                    racksCount++;
+                    capacityVariable -= racks.Pop();
+                    capacityVariable = capacity;
+                }
+                else
+                {
+                    capacityVariable -= racks.Pop();
+                    if (racks.Count == 0)
+                    {
+                        racksCount++;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(racksCount);
+
+        }
+    }
+}
---
+++ C# Advanced/C# Advanced - Exercises/Stack and Queues - Exercises/SongsQueue/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SongsQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Queue<string> songs = new Queue<string>(input);
+
+            string command = Console.ReadLine();
+
+
+            while (songs.Count > 0)
+            {
+                if (command.Contains("Play"))
+                {
+                    songs.Dequeue();
+                }
+                else if (command.Contains("Add"))
+                {
+                    int indexOne = command.IndexOf("Add");
+                    command = command.Remove(indexOne, 4);
+
+                    if (songs.Contains(command))
+                    {
+                        Console.WriteLine($"{command} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(command);
+                    }
+                }
+                else if (command.Contains("Show"))
+                {
+                    Console.WriteLine($"{string.Join(", ", songs)}");
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine("No more songs!");
+
+        }
+    }
+}
