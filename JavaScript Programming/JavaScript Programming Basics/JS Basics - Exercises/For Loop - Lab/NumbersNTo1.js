1be1ab61c2db731a17619e0dac5b87b2a141a4f8
Daniel <kace123@abv.bg>
2021-09-13 18:51:27 +0300

    Added exercises to JS Programming

---
+++ JavaScript Programming/JavaScript Programming Basics/JS Basics - Exercises/For Loop - Lab/NumbersNTo1.js
@@ -0,0 +1,9 @@
+function PrintNumbersNTo1(n) {
+
+    for (let i = n; i >= 1; i--) {
+        console.log(i);
+    }
+}
+
+
+PrintNumbersNTo1('100');
---
+++ JavaScript Programming/JavaScript Programming Basics/JS Basics - Exercises/For Loop - Lab/NumbersToNPlus3.js
@@ -0,0 +1,8 @@
+function PrintNumbersPlusThree(n) {
+
+    for (let i = 1; i <= n; i += 3) {
+        console.log(i);
+    }
+}
+
+PrintNumbersPlusThree(10);
