ddb18edbff6006ad616b0f1e56e4db1621fc9e4d
Daniel Nikolov <kace123@abv.bg>
2021-09-14 22:31:55 +0300

    Added exercises to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Multidimensional Arrays - Lab/SumMatrixColumns/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace SumMatrixColumns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] sizes = ReadInput();
+
+            int[,] matrix = new int[sizes[0], sizes[1]];
+
+            GetSum(AddElementsToMatrix(matrix, sizes[0], sizes[1]));
+
+        }
+        static void GetSum(int[,] matrix)
+        {
+
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                int sum = 0;
+
+                for (int j = 0; j < matrix.GetLength(0); j++)
+                {
+                    sum += matrix[j, i];
+                }
+
+                Console.WriteLine(sum);
+
+            }
+        }
+        public static int[,] AddElementsToMatrix(int[,] matrix, int row, int col)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                int[] arr = ReadInput();
+
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = arr[j];
+                }
+            }
+
+            return matrix;
+        }
+        public static int[] ReadInput()
+        {
+            return Console.ReadLine()
+                .Split(new string[] { ", ", " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
