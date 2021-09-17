aeb85145d689fceebe90499cedd9f07e6a52b263
Daniel <kace123@abv.bg>
2021-09-17 23:17:03 +0300

    Added exercies to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/AverageStudentGrades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<double>> studentGrades = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string name = input[0];
+                double grade = double.Parse(input[1]);
+
+                if (studentGrades.ContainsKey(name))
+                {
+                    studentGrades[name].Add(grade);
+                } 
+                else
+                {
+                    studentGrades.Add(name, new List<double>() { grade });
+                }
+            }
+
+            foreach (var student in studentGrades)
+            {
+                Console.WriteLine($"{student.Key} -> {string.Join(" ", student.Value)} (avg: {student.Value.Average():f2})");
+            }
+        }
+    }
+}
