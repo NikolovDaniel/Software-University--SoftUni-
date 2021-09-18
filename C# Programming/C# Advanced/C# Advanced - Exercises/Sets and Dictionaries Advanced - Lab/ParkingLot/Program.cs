0267f871a5d7819df020e0a2a8a8c2db91737123
Daniel <kace123@abv.bg>
2021-09-19 00:09:45 +0300

    Added exercise to C# Programming

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Lab/ParkingLot/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> cars = new HashSet<string>();
+
+            string[] input = Console.ReadLine().Split(", ");
+
+            while (input[0] != "END")
+            {
+
+                if (input[0] == "IN")
+                {
+                    cars.Add(input[1]);
+                }
+                else if (input[0] == "OUT")
+                {
+                    cars.Remove(input[1]);
+                }
+
+                input = Console.ReadLine().Split(", ");
+            }
+
+            if (cars.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+                return;
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
