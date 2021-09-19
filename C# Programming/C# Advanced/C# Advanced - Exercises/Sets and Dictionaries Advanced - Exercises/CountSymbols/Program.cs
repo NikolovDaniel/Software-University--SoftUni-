f4bdfb9613ce7186ee37fed287f9197e72cee3d9
Daniel <kace123@abv.bg>
2021-09-19 19:43:51 +0300

    Added exercise to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercises/CountSymbols/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountSymbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (charCount.ContainsKey(input[i]))
+                {
+                    charCount[input[i]]++;
+                }
+                else
+                {
+                    charCount.Add(input[i], 1);
+                }
+            }
+
+            foreach (var ch in charCount.OrderBy(x => x.Key))
+            {
+                Console.WriteLine($"{ch.Key}: {ch.Value} time/s");
+            }
+
+        }
+    }
+}
