2587bd0f2086bc34dc1014b6e8b88ec446f6b2ff
Daniel <kace123@abv.bg>
2021-09-19 20:00:00 +0300

    Added exercise to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Sets and Dictionaries Advanced - Exercises/EvenTimes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EvenTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<double, int> numbersCount = new Dictionary<double, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double num = double.Parse(Console.ReadLine());
+
+                if (numbersCount.ContainsKey(num))
+                {
+                    numbersCount[num]++;
+                }
+                else
+                {
+                    numbersCount.Add(num, 1);
+                }
+            }
+
+            foreach (var num in numbersCount.Where(x => x.Value % 2 == 0))
+            {
+                Console.WriteLine(num.Key);
+            }
+        }
+    }
+}
