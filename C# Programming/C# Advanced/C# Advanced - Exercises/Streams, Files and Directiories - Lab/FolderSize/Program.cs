de4220c3b8db495d59f3faae594a9682fdd3f87f
Daniel Nikolov <kace123@abv.bg>
2021-09-22 22:43:52 +0300

    Added exercise to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Streams, Files and Directiories - Lab/FolderSize/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] folderFiles = Directory.GetFiles(@"C:\Users\KOlombus\source\repos\CSharp Advanced\FolderSize\TestFolder\");
+
+            double sum = 0;
+
+            for (int i = 0; i < folderFiles.Length; i++)
+            {
+                FileInfo fileInfo = new FileInfo(folderFiles[i]);
+                sum += fileInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+
+            File.WriteAllText("output.txt", sum.ToString());
+        }
+    }
+}
