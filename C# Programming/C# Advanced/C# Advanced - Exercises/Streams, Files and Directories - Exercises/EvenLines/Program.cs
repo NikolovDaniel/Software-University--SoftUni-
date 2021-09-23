0977a797900254f78bbfa477ee2fba65aa894993
Daniel Nikolov <kace123@abv.bg>
2021-09-23 22:07:11 +0300

    Added exercise to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Streams, Files and Directories - Exercises/EvenLines/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EvenLines
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (StreamReader str = new StreamReader("text.txt"))
+            {
+                using (StreamWriter swtr = new StreamWriter("output.txt"))
+                {
+                    string line = await str.ReadLineAsync();
+                    int counter = 0;
+
+                    while (line != null)
+                    {
+                        if (line.Contains('-'))
+                        {
+                            line = line.Replace('-', '@');
+                        }
+                        if (line.Contains(','))
+                        {
+                            line = line.Replace(',', '@');
+                        }
+                        if (line.Contains('.'))
+                        {
+                            line = line.Replace('.', '@');
+                        }
+                        if (line.Contains('!'))
+                        {
+                            line = line.Replace('!', '@');
+                        }
+                        if (line.Contains('?'))
+                        {
+                            line = line.Replace('?', '@');
+                        }
+
+                        if (counter % 2 == 0)
+                        {
+                            string result = string.Empty;
+
+                            string[] arr = line.Split();
+
+                            for (int i = arr.Length - 1; i >= 0; i--)
+                            {
+                                result += $"{arr[i]} ";
+                            }
+
+                            await swtr.WriteLineAsync(result.TrimEnd());
+                        }
+                        counter++;
+                        line = await str.ReadLineAsync();
+                    }
+                }
+            }
+        }
+    }
+}
