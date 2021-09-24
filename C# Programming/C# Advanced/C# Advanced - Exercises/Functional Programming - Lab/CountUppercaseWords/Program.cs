baf2cf8a6a566b3098234ebbd543104835004d83
Daniel Nikolov <kace123@abv.bg>
2021-09-24 22:13:22 +0300

    Added exercise to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Functional Programming - Lab/CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string, bool> predicate = str => char.IsUpper(str[0]);
+
+            Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Where(predicate)
+                    .ToList()
+                    .ForEach(w => Console.WriteLine(w));
+        }
+    }
+}
