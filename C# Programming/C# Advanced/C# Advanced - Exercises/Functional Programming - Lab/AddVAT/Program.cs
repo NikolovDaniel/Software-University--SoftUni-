55d4133999ad064e6d5fd8f5d5b2d3d72fa4a835
Daniel Nikolov <kace123@abv.bg>
2021-09-24 22:13:03 +0300

    Added exercise to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Functional Programming - Lab/AddVAT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Func<string, double> parser = n => double.Parse(n);
+            Func<double, double> addVat = n => n += n * 0.20;
+
+            Console.ReadLine()
+                .Split(", ")
+                .Select(parser)
+                .Select(addVat)
+                .ToList()
+                .ForEach(n => Console.WriteLine($"{n:f2}"));
+        }
+    }
+}
