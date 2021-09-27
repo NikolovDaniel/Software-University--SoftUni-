4a154ae2048c7fe17d136ffd820360c23d8fd029
Daniel Nikolov <kace123@abv.bg>
2021-09-27 22:21:21 +0300

    Added exercises to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Defining Classes - Exercises/DateModify/DateModifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DateModify
+{
+    class DateModifier
+    {
+        public double Days { get; set; }
+
+        public void DayDifference(DateTime timeOne, DateTime timeTwo)
+        {
+            Func<DateTime, DateTime, double> difference = (timeOne, timeTwo) => timeOne > timeTwo ? (timeOne - timeTwo).TotalDays : (timeTwo - timeOne).TotalDays;
+
+            this.Days = difference(timeOne, timeTwo);
+        }
+    }
+}
---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Defining Classes - Exercises/DateModify/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DateModify
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] timeOne = Console.ReadLine().Split();
+            string[] timeTwo = Console.ReadLine().Split();
+
+            DateTime dateOne = new DateTime(int.Parse(timeOne[0]), int.Parse(timeOne[1]), int.Parse(timeOne[2]));
+            DateTime dateTwo = new DateTime(int.Parse(timeTwo[0]), int.Parse(timeTwo[1]), int.Parse(timeTwo[2]));
+
+            DateModifier modifier = new DateModifier();
+
+            modifier.DayDifference(dateOne, dateTwo);
+
+            Console.WriteLine(modifier.Days);
+        }
+    }
+}
