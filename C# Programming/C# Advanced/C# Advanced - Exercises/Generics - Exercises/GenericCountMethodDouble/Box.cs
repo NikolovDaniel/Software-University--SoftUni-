3fd0a60b1c973f6cf692e92a859921ec3cdaade7
Daniel Nikolov <kace123@abv.bg>
2021-10-01 21:24:49 +0300

    Added exercise to C# Advanced - Generics - Exercises

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Generics - Exercises/GenericCountMethodDouble/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenericCountMethodDouble
+{
+    public class Box<T> where T : IComparable
+    {
+        public List<T> Elements { get; }
+       
+        public Box(List<T> elements)
+        {
+            this.Elements = elements;
+        }
+        
+        public int CountGreaterElements(List<T> elements, T element) 
+        {
+            return elements.Where(e => e.CompareTo(element) > 0).ToList().Count;
+        }
+
+
+    }
+}
---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Generics - Exercises/GenericCountMethodDouble/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericCountMethodDouble
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<double> list = new List<double>();
+
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+                list.Add(input);
+            }
+
+            Box<double> listElements = new Box<double>(list);
+
+            double inpt = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(listElements.CountGreaterElements(list, inpt));
+        }
+    }
+}
