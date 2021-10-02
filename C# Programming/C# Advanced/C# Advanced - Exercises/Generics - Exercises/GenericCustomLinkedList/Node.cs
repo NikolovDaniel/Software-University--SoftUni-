f5073c523cdf0f1a3dd3666e7ef230e476934dd8
Daniel Nikolov <kace123@abv.bg>
2021-10-02 14:12:03 +0300

    Added exercise to C# Advanced - Generics - Exercises

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Generics - Exercises/GenericCustomLinkedList/Node.cs
@@ -0,0 +1,9 @@
+﻿namespace GenericCustomLinkedList
+{
+    public class Node<T>
+    {
+        public T Value { get; set; }
+        public Node<T> Next { get; set; }
+        public Node<T> Previous { get; set; }
+    }
+}
---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Generics - Exercises/GenericCustomLinkedList/StartUp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GenericCustomLinkedList
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            CustomLinkedList<string> list = new CustomLinkedList<string>(new string[] { "Pesho", "Gosho", "Petko" });
+
+            list.AddFirst("Daniel");
+            list.AddLast("Nikolov");
+
+            list.ForEach(n => Console.WriteLine(n));
+
+            list.RemoveFirst();
+            list.RemoveLast();
+
+            string[] arr = list.ToArray();
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
