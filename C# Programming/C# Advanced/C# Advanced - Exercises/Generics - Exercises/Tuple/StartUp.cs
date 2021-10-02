06dfd2af9327366971ca43523cb69101bd2b484a
Daniel Nikolov <kace123@abv.bg>
2021-10-02 13:54:51 +0300

    Added exercise to C# Advanced - Generics - Exercises

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Generics - Exercises/Tuple/StartUp.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tuple
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+
+            string fullName = $"{firstInput[0]} {firstInput[1]}";
+            string adress = firstInput[2];
+
+            string[] secondInput = Console.ReadLine().Split();
+
+            string name = secondInput[0];
+            int litersBeer = int.Parse(secondInput[1]);
+
+            string[] thirdInput = Console.ReadLine().Split();
+
+            int integer = int.Parse(thirdInput[0]);
+            double numDouble = double.Parse(thirdInput[1]);
+
+            Tuple<string, string> firstTuple = new Tuple<string, string>(fullName, adress);
+            Tuple<string, int> secondTuple = new Tuple<string, int>(name, litersBeer);
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(integer, numDouble);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Generics - Exercises/Tuple/Tuple.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tuple
+{
+    public class Tuple<TFirst, TSecond>
+    {
+
+        public TFirst ElementOne { get; private set; }
+        public TSecond ElementTwo { get; private set; }
+
+        public Tuple(TFirst elementOne, TSecond elementTwo)
+        {
+            this.ElementOne = elementOne;
+            this.ElementTwo = elementTwo;
+        }
+
+        public override string ToString()
+        {
+            return $"{ElementOne} -> {ElementTwo}";
+        }
+    }
+}
