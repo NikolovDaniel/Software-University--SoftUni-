3ab3d240fd2872df772257265c76c674787d4d18
Daniel Nikolov <kace123@abv.bg>
2021-10-03 20:38:29 +0300

    Added exercise to C# Advanced

---
+++ C# Programming/C# Advanced/C# Advanced - Exercises/Iterators and Comparators - Lab/LibraryMain/Library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryMain
+{
+    public class Library
+    {
+
+        public List<Book> books { get; private set; }
+
+        public Library(params Book[] _books)
+        {
+            this.books = new List<Book>(_books);
+        }
+    }
+}
