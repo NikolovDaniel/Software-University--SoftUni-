88eb50177f931bd3293b02627587c33d34f8d246
Daniel Nikolov <kace123@abv.bg>
2021-10-20 15:51:28 +0300

    Added exercise to C# Advanced - Exams

---
+++ C# Programming/C# Advanced/C# Advanced - Exams/C# - Advanced Retake Exam - 19 August 2020/VetClinic/Pet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Pet
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Owner { get; set; }
+
+        public Pet(string name, int age, string owner)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Owner = owner;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Name: {this.Name} Age: {this.Age} Owner: {this.Owner}");
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
