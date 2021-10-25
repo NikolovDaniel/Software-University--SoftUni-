7b2b497630f99ea43cb3b8ee83faa2d78aea88e3
Daniel Nikolov <kace123@abv.bg>
2021-10-25 22:07:04 +0300

    Added exercise to C# OOP - Inheritance - Lab

---
+++ C# Programming/C# OOP/Inheritance - Lab/CustomRandomList/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] items = new string[] { "a", "b", "c" };
+            RandomList list = new RandomList(items);
+
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
