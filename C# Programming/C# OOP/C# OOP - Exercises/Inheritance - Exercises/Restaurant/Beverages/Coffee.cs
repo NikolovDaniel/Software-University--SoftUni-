35a9c15616462e024857abd35a3a6f804fc1efa4
Daniel Nikolov <kace123@abv.bg>
2021-10-27 17:33:33 +0300

    Added exercise to C# OOP - Inheritance - Exercises

---
+++ C# Programming/C# OOP/C# OOP - Exercises/Inheritance - Exercises/Restaurant/Beverages/Coffee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double CoffeeMilliliters = 50;
+        private const decimal CoffeePrice = 3.50M;
+        public override double Milliliters { get => base.Milliliters; }
+        public override decimal Price { get => base.Price; }
+        public double Caffeine { get; set; }
+
+
+        public Coffee(string name, double caffeine)
+            : base(name, 0, 0)
+        {
+            this.Caffeine = caffeine;
+        }
+    }
+}
