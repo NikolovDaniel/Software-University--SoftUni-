b33fa4109cbcfcea13269bae755f0b091177fa0b
Daniel Nikolov <kace123@abv.bg>
2021-10-28 22:13:05 +0300

    Added exercise to C# OOP - Encapsulation - Lab

---
+++ C# Programming/C# OOP/C# OOP - Exercises/Encapsulation - Lab/Salary/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Salary
+{
+    public class Person
+    {
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            decimal increase = percentage;
+            if (this.Age < 30) increase /= 2;
+            Salary += Salary * (increase / 100);
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+
+    }
+}
---
+++ C# Programming/C# OOP/C# OOP - Exercises/Encapsulation - Lab/Salary/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Salary
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+            int lines = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < lines; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                Person person = new Person(input[0], input[1], int.Parse(input[2]), decimal.Parse(input[3]));
+
+                people.Add(person);
+            }
+
+            decimal percentage = decimal.Parse(Console.ReadLine());
+
+            people.ForEach(p => p.IncreaseSalary(percentage));
+            people.ForEach(p => Console.WriteLine(p));
+
+        }
+    }
+}
