164ab7f680c6ba98f563f43270ac6af9912f7492
Daniel Nikolov <kace123@abv.bg>
2021-10-29 22:06:44 +0300

    Added exercise to C# OOP - Encapsulation Exercises

---
+++ C# Programming/C# OOP/C# OOP - Exercises/Encapsulation - Exercises/ClassBoxData/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ClassBoxData
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+
+
+            try
+            {
+                Box box = new Box(length, width, height);
+
+                box.SurfaceArea();
+                box.LateralSurfaceArea();
+                box.Volume();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        } 
+    }
+}
