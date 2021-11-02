f06a54552d453aae1c5131c338cf9d4b488fbbfd
Daniel <kace123@abv.bg>
2021-11-02 22:11:12 +0200

    Added exercise to C# OOP - Interfaces and Abstractions - Exercises

---
+++ C# Programming/C# OOP/C# OOP - Exercises/Interfaces and Abstractions - Exercises/BorderControl/Creatures.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public abstract class Creatures
+    {
+        public string Id { get; private set; }
+
+        public Creatures(string id)
+        {
+            this.Id = id;
+        }
+    }
+}
---
+++ C# Programming/C# OOP/C# OOP - Exercises/Interfaces and Abstractions - Exercises/BorderControl/Robot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BorderControl
+{
+    public class Robot : Creatures
+    {
+        public string Model { get; private set; }
+        public Robot(string model, string id)
+            : base(id)
+        {
+            this.Model = model;
+        }
+    }
+}
