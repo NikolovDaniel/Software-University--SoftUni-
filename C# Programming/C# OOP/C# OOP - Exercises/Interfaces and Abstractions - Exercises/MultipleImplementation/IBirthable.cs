ab69bd3c858402140322a7ef98883ce828dc75f3
Daniel <kace123@abv.bg>
2021-11-02 22:12:20 +0200

    Added exercise to C# OOP - Interfaces and Abstractions - Exercises

---
+++ C# Programming/C# OOP/C# OOP - Exercises/Interfaces and Abstractions - Exercises/MultipleImplementation/IBirthable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultipleImplementation
+{
+    public interface IBirthable
+    {
+        public string Birthdate { get; }
+    }
+}
