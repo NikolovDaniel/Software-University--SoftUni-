3186c34e2da1d24a2d691a25a26145514dac9613
Daniel <kace123@abv.bg>
2021-11-04 20:52:39 +0200

    Added exercise to C# OOP - Interfaces and Abstractions - Exercises

---
+++ C# Programming/C# OOP/C# OOP - Exercises/Interfaces and Abstractions - Exercises/CollectionHierarchy/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CollectionHierarchy
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            AddCollection addCollection = new AddCollection();
+            AddRemoveCollection addRemoveCollection = new AddRemoveCollection();
+            MyList myList = new MyList();
+
+            List<int> indexAddCollection = new List<int>();
+            List<int> indexAddRemoveCollection = new List<int>();
+            List<int> indexMyList = new List<int>();
+
+            string[] items = Console.ReadLine().Split();
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                indexAddCollection.Add(addCollection.Add(items[i]));
+                indexAddRemoveCollection.Add(addRemoveCollection.Add(items[i]));
+                indexMyList.Add(myList.Add(items[i]));
+            }
+
+            int n = int.Parse(Console.ReadLine());
+
+            List<string> resultAddRemoveCollection = new List<string>();
+            List<string> resultMyList = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                resultAddRemoveCollection.Add(addRemoveCollection.Remove());
+                resultMyList.Add(myList.Remove());
+            }
+            Console.WriteLine(string.Join(" ", indexAddCollection));
+            Console.WriteLine(string.Join(" ", indexAddRemoveCollection));
+            Console.WriteLine(string.Join(" ", indexMyList));
+            Console.WriteLine(string.Join(" ", resultAddRemoveCollection));
+            Console.WriteLine(string.Join(" ", resultMyList));
+        }
+    }
+}
