52b861cde7c6aaf6054f97960848503b7de62270
Daniel <kace123@abv.bg>
2021-11-04 20:52:19 +0200

    Added exercise to C# OOP - Interfaces and Abstractions - Exercises

---
+++ C# Programming/C# OOP/C# OOP - Exercises/Interfaces and Abstractions - Exercises/MilitaryElite/Interfaces/ILeutenantGeneral.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite
+{
+    public interface ILeutenantGeneral
+    {
+        public List<Soldier> Privates { get; }
+    }
+}
---
+++ C# Programming/C# OOP/C# OOP - Exercises/Interfaces and Abstractions - Exercises/MilitaryElite/Models/SpecialisedSoldier.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MilitaryElite
+{
+    public abstract class SpecialisedSoldier : Private, ISpecialisedSoldier
+    {
+        private string corps;
+
+        public string Corps
+        {
+            get { return corps; }
+            set
+            {
+                if (value != "Marines" && value != "Airforces")
+                {
+                    throw new ArgumentException();
+                }
+                corps = value;
+            }
+        }
+
+
+        public SpecialisedSoldier(string id, string firstName, string lastName, decimal salary, string corps)
+            : base(id, firstName, lastName, salary)
+        {
+            this.Corps = corps;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.ToString())
+                .AppendLine($"Corps: {Corps}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
