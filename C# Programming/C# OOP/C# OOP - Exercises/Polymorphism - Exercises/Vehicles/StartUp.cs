3d32ce26ad0ffa3d020fe865a5a9aa734c921640
Daniel <kace123@abv.bg>
2021-11-06 11:59:38 +0200

    Added exercise to C# - OOP - Polymorphism - Exercises

---
+++ C# Programming/C# OOP/C# OOP - Exercises/Polymorphism - Exercises/Vehicles/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Vehicles
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] carInfo = Console.ReadLine().Split();
+            string[] truckInfo = Console.ReadLine().Split();
+
+            Car car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]));
+            Truck truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]));
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (input[0] == "Drive")
+                {
+                    if (input[1] == "Car")
+                    {
+                        Console.WriteLine(car.Drive(double.Parse(input[2])));
+                    }
+                    else
+                    {
+                        Console.WriteLine(truck.Drive(double.Parse(input[2])));
+                    }
+                }
+                else
+                {
+                    if (input[1] == "Car")
+                    {
+                        car.Refuel(double.Parse(input[2]));
+                    }
+                    else
+                    {
+                        truck.Refuel(double.Parse(input[2]));
+                    }
+                }
+            }
+
+            Console.WriteLine(car.ToString());
+            Console.WriteLine(truck.ToString());
+        }
+    }
+}
