72d44e33cdaa19f13ca66880126294a6c83c00bc
Daniel Nikolov <kace123@abv.bg>
2021-11-11 19:23:56 +0300

    Added exercise to C# OOP - Polymorphism - Exercises

---
+++ C# Programming/C# OOP/C# OOP - Exercises/Polymorphism - Exercises/WildFarm/Owl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WildFarm
+{
+    public class Owl : Bird
+    {
+        public Owl(string name, double weight, double wingSize)
+            : base(name, weight, wingSize)
+        {
+        }
+
+        public void ProducingSound()
+        {
+            Console.WriteLine("Hoot Hoot");
+        }
+        public void EatFood(int quantity)
+        {
+            this.Weight += quantity * 0.25;
+            this.FoodEaten += quantity;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.GetType().Name} [{this.Name}, {this.WingSize}, {this.Weight}, {this.FoodEaten}]";
+        }
+    }
+}
