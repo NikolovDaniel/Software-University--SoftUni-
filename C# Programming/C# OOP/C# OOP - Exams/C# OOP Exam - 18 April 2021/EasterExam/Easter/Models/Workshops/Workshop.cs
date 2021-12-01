34385837210119242f7de23b30efbb424d88baea
Daniel Nikolov <kace123@abv.bg>
2021-12-01 21:01:16 +0300

    Added exercise to C# OOP - Exams

---
+++ C# Programming/C# OOP/C# OOP - Exams/C# OOP Exam - 18 April 2021/EasterExam/Easter/Models/Workshops/Workshop.cs
@@ -0,0 +1,44 @@
+﻿using Easter.Models.Bunnies.Contracts;
+using Easter.Models.Dyes;
+using Easter.Models.Dyes.Contracts;
+using Easter.Models.Eggs.Contracts;
+using Easter.Models.Workshops.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easter.Models.Workshops
+{
+    public class Workshop : IWorkshop
+    {
+        public void Color(IEgg egg, IBunny bunny)
+        {
+
+            List<IDye> dyes = bunny.Dyes.ToList();
+
+            if (bunny.Energy > 0 && dyes.Count > 0)
+            {
+                IDye dye = bunny.Dyes.FirstOrDefault();
+
+                while (!egg.IsDone() && bunny.Energy > 0 && bunny.Dyes.Count > 0)
+                {
+                    if (dye.Power == 0)
+                    {
+                        bunny.Dyes.Remove(dye);
+                        dye = bunny.Dyes.FirstOrDefault();
+                    }
+
+                    if (dye == null)
+                    {
+                        break;
+                    }
+
+                    dye.Use();
+                    bunny.Work();
+                    egg.GetColored();
+                }
+            }
+        }
+    }
+}
