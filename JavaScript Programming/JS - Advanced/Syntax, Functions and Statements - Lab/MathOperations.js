b8d35ba0c84773eb9cb007e724331ff6cc5bf664
Daniel Nikolov <kace123@abv.bg>
2021-12-19 19:22:02 +0300

    Added exercise to JS - Advanced

---
+++ JavaScript Programming/JS - Advanced/Syntax, Functions and Statements - Lab/MathOperations.js
@@ -0,0 +1,23 @@
+function mathOperations(numOne, numTwo, operator) {
+
+    let result;
+
+    if (operator == '+') {
+        result = numOne + numTwo;
+    } else if (operator == '-') {
+        result = numOne - numTwo;
+    } else if (operator == '*') {
+        result = numOne * numTwo;
+    } else if (operator == '/') {
+        result = numOne / numTwo;
+    } else if (operator == '%') {
+        result = numOne % numTwo;
+    } else if (operator == '**') {
+        result = numOne ** numTwo;
+    }
+
+    console.log(result);
+}
+
+mathOperations(5, 6, '+');
+mathOperations(3, 5.5, '*');
