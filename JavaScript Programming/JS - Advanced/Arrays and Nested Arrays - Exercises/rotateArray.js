61ce1c545be0040fc27f5272e2877d66bcebf0a6
Daniel Nikolov <kace123@abv.bg>
2022-01-18 20:17:29 +0300

    Added exercise to JS Advanced

---
+++ JavaScript Programming/JS - Advanced/Arrays and Nested Arrays - Exercises/rotateArray.js
@@ -0,0 +1,11 @@
+function solve(arr, rotations) {
+
+    for (let i = 0; i < rotations % arr.length; i++) {
+
+        arr.unshift(arr.pop());
+    }
+
+    console.log(arr.join(' '));
+}
+
+solve(['1', '2', '3', '4'], 2);
